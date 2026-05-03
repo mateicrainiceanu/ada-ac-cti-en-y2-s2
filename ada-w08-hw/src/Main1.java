82a0fac2ccfb72eb22e12a1beccc7481a1e72daa
mateicrainiceanu <matei.crainiceanu@gmail.com>
2026-05-03 17:18:29 +0300

    hw-08-1-1-done

--- ada-w08-hw/src/Main1.java
+++ ada-w08-hw/src/Main1.java
@@ -1,12 +1,12 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class Main1 {
 
     public static void main(String[] args) {
         String FILENAME = "ada-w08-hw/people1.txt";
 
         PeopleGraph peopleGraph = PeopleGraph.fromFile(FILENAME);
-
+        peopleGraph.printBiggestInfluenceGroup();
 
     }
 }
--- ada-w08-hw/src/PeopleGraph.java
+++ ada-w08-hw/src/PeopleGraph.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class PeopleGraph {
@@ -13,36 +15,31 @@ public class PeopleGraph {
         this.people = new ArrayList<>(count);
     }
 
-    static PeopleGraph fromFile (String filename) {
+    static PeopleGraph fromFile(String filename) {
         try {
             Scanner input = new Scanner(new File(filename));
 
-
-            // int
             int count = input.nextInt();
-
-            PeopleGraph graph = new PeopleGraph(count);
+            PeopleGraph pg = new PeopleGraph(count);
 
             input.nextLine();
             while (input.hasNextLine() && count > 0) {
-                graph.addPerson(input.nextLine());
+                pg.addPerson(input.nextLine());
                 count--;
             }
 
             while (input.hasNextLine()) {
                 String line = input.nextLine();
                 if (line.isEmpty()) continue;
-                String [] tokens = line.split(" ");
-                graph.addConnection(tokens[0], tokens[1]);
+                String[] tokens = line.split(" ");
+                pg.addConnection(tokens[0], tokens[1]);
             }
 
-            graph.print();
+            return pg;
 
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
-
-        return null;
     }
 
     public void addPerson(String person) {
@@ -50,10 +47,46 @@ public class PeopleGraph {
     }
 
     public void addConnection(String person1, String person2) {
-        int idx1 =  people.indexOf(person1);
+        int idx1 = people.indexOf(person1);
         int idx2 = people.indexOf(person2);
-
         graph.addUndirectedEdge(idx1, idx2);
+    }
+
+    // BFS from startNode, marks visited nodes and returns the component members
+    private ArrayList<String> bfsComponent(int startNode, boolean[] visited) {
+        ArrayList<String> component = new ArrayList<>();
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(startNode);
+        visited[startNode] = true;
+
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            component.add(people.get(node));
+            for (int neighbor : graph.nodesAdjacentTo(node)) {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    queue.add(neighbor);
+                }
+            }
+        }
+        return component;
+    }
+
+    public void printBiggestInfluenceGroup() {
+        int n = people.size();
+        boolean[] visited = new boolean[n];
+        ArrayList<String> biggest = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                ArrayList<String> component = bfsComponent(i, visited);
+                if (component.size() > biggest.size()) {
+                    biggest = component;
+                }
+            }
+        }
+
+        System.out.println("Biggest group of influence - size: " + biggest.size());
     }
 
     public void print() {
